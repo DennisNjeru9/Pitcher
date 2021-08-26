5ae6445791013837893cd44cd7cd65d15b0be68c
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-26 12:12:57 +0300

    create a welcome message for users in the application

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from ..models import User
 from .forms import LoginForm,RegistrationForm
 from flask_login import login_user,logout_user,login_required
 from ..import db
+from ..email import mail_message
 
 @auth.route('/login')
 def login():
@@ -28,7 +29,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        #mail_message("Welcome to Pitcher","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Pitcher","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
