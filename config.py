28404e3f38903b5a2a7d265029ad0deded67b73a
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-26 11:55:11 +0300

    setup the SMTP server and configure port for use by the gmail SMTP server port and enable transport layer security to secure emails when sending them

--- config.py
+++ config.py
@@ -4,6 +4,12 @@ class Config:
     SECRET_KEY = 'thisissecret'
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:Access@localhost/pitchcollections'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     pass
