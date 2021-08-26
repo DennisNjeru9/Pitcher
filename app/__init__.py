37366f50cdfa03667e9502ec8f80867a189a4d6b
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-26 09:57:20 +0300

    import login manager extension and create its instance. Initialize the login manager extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_manager
 
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -12,6 +17,7 @@ def create_app(config_name):
 
     #initializing flask extensions
     db.init_app(app)
+    login_manager.init_app(app)
 
     #registering blueprint
     from .main import main as main_blueprint
