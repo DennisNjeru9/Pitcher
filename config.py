f1199c723197d8e9422ef33a08dc159c534be9ef
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-26 16:49:23 +0300

    add test-cases for user behaviors of the user and comment models

--- config.py
+++ config.py
@@ -10,15 +10,23 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    #simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
     pass
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:Access@localhost/pitchcollections_test'
+
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:Access@localhost/pitchcollections'
     DEBUG = True
 
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
--- manage.py
+++ manage.py
@@ -1,19 +1,26 @@
 from app import create_app,db
-from flask_script import Manager, Server
+from flask_script import Manager,Server
 from app.models import User,Role,Comment
 from flask_migrate import Migrate,MigrateCommand
 
-app = create_app('development')
+app = create_app('test')
 
 migrate = Migrate(app,db)
 manager = Manager(app)
 manager.add_command('db',MigrateCommand)
 #manager.add_command('server',Server)
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User, Role = Role)
+    return dict(app = app, db = db, User = User, Role = Role, Comment = Comment)
 
 
 if __name__ == '__main__':
