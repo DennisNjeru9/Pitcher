eb758236da51d331ea9a684b07f17ab2c030851f
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-26 07:41:40 +0300

    add sqlaclchemy database uri to the configuration class

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    SECRET_KEY = 'this is secret'
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:Access@localhost/pitches'
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
