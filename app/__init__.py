213ca12c0923c382b4c0e03767585735878f2d13
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-26 09:48:58 +0300

    register the authentication blueprint and create a login function to render the login template

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .main import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
     #setting config
     #from .requests import configure_request
     #configure_request(app)
