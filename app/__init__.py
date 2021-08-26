a974573bfb1a575f07a71a5c2ed2a99f6abee9cb
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-26 11:11:46 +0300

    add the auth register template and initialize the bootstrap extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import config_options
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 login_manager = LoginManager()
@@ -16,6 +18,7 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     #initializing flask extensions
+    bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
 
